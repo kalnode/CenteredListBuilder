cb7d298dc103c5eb3f4c9d6ecf926a5f00a2efd1
Kal Erikson <hello@bizjelly.com>
2020-01-27 12:26:36 -0500

    MON JAN 27 CLEANUP

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 function Footer() {
   return (
-    <div id="footer">Copyright &copy; {(new Date().getFullYear())}&nbsp;<a href="https://kreation.ca">Kal Erikson</a></div>
+    <div id="footer">
+      <div>Copyright &copy; {(new Date().getFullYear())}&nbsp;<a href="https://kreation.ca">Kal Erikson</a> • <a href="https://github.com/TheMangoTrain/centeredliststyler">GitHub</a></div>
+    </div>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import Logo from '../components/Logo';
+import GithubIcon from '../assets/github.svg';
 
 function Header(props) {
   return (
     <>
-    <NavLink activeClassName='is-active' to='/'>
-    <div style={{margin: "1em",textAlign: "center"}}>
-      <Logo />
-      <div>
-        <h1 style={{marginTop: "0em", marginBottom: "0.3em"}}>Centered-list</h1>
-        <h2 style={{marginTop: "0em"}}>Builder</h2>
+    <NavLink activeClassName='is-active' to='/about'>
+      <div style={{margin: "1em",textAlign: "center"}}>
+        <Logo />
+        <div>
+          <h1 style={{marginTop: "0em", marginBottom: "0.3em"}}>Centered-list</h1>
+          <h2 style={{marginTop: "0em"}}>Builder v1</h2>
+        </div>
       </div>
-    </div>
     </NavLink>
 
     <ul id="navigation">
       <li><NavLink exact={true} activeClassName='is-active' to='/about'>About</NavLink></li>
       <li><NavLink activeClassName='is-active' to='/tool'>Make a List</NavLink></li>
     </ul>
+
+    <div id="headerGithub">
+      <a href="https://github.com/TheMangoTrain/centeredliststyler"><img style={{height: "2em"}} src={GithubIcon} className="logo" alt="logo" /> GitHub</a>
+    </div>
     </>
   );
 }
