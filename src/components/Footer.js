899d66d3677ed0c9719e832dda4d1c43db4b15b0
Kal Erikson <hello@bizjelly.com>
2020-01-27 11:55:24 -0500

    MON JAN 27 CLEANUP

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
   return (
-    <div id="footer">Copyright &copy; {(new Date().getFullYear())} <a href="https://kreation.ca">Kal Erikson</a></div>
+    <div id="footer">Copyright &copy; {(new Date().getFullYear())}&nbsp;<a href="https://kreation.ca">Kal Erikson</a></div>
   );
 }
 
