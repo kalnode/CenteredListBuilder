86fc84d785626fe135598ced703e90b835957218
Kal <kounapuu@gmail.com>
2023-04-03 12:15:16 -0400

    Updates

---
+++ precache-manifest.6a7f2162a303cfec4b4d3233c882b868.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa8167c87b788be9df409670de78696e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64f80154339fd7ca8585",
+    "url": "/static/css/main.18fb393f.chunk.css"
+  },
+  {
+    "revision": "e12ca6b6aa641a328926",
+    "url": "/static/js/2.1d713802.chunk.js"
+  },
+  {
+    "revision": "64f80154339fd7ca8585",
+    "url": "/static/js/main.e85e7ed1.chunk.js"
+  },
+  {
+    "revision": "063482b99c2db0c77905",
+    "url": "/static/js/runtime-main.3c9ee553.js"
+  },
+  {
+    "revision": "edd19191295a728a394f86b3c4821a98",
+    "url": "/static/media/github.edd19191.svg"
+  },
+  {
+    "revision": "12fd5c1290eec55c42ea4a5b822a58b3",
+    "url": "/static/media/logo.12fd5c12.svg"
+  }
+]);
